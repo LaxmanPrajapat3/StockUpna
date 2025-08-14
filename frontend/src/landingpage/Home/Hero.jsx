14d1b0474e349f692c1c0023d5496aed53643f4b
Laxman <theprogrammer452023@gmail.com>
2025-08-14 08:59:26 +0530

    improve home page

--- frontend/src/landingpage/Home/Hero.jsx
+++ frontend/src/landingpage/Home/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero(){
     return(
         <>
-         <section className="text-center max-w-2xl mx-auto mt-46 px-2 ">
+         <section className="text-center max-w-2xl mx-auto mt-46 px-2 mb-20">
       <h1 className="text-3xl sm:text-4xl font-bold text-gray-900 mb-4">
         Learn about investing <br className="hidden sm:block" /> with confidence
       </h1>
@@ -9,12 +9,10 @@ export default function Hero(){
         A guiding hands-on platform for new investors to gain knowled
         about the stock market and track their portfolios
       </p>
-      {/* <button className="bg-teal-600 text-white font-semibold py-2 px-6 rounded hover:bg-teal-700 cursor-not-allowed">
-        Get Started
-      </button> */}
       <button className="bg-teal-600 text-white font-semibold py-2 px-6 rounded hover:bg-teal-700 cursor-not-allowed">
-  Get Started
-</button>
+        Get Started
+      </button>
+     
 
     </section>
         </>
