aaf213003fde47e5016c0bccdb9ee7ea775b4ca9
Laxman <theprogrammer452023@gmail.com>
2025-08-14 23:54:56 +0530

    improve vite config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import dotenv from "dotenv";
+dotenv.config();
 // https://vite.dev/config/
 export default defineConfig({
    theme: {
     extend: {},
   },
+  base:"./",
   plugins: [react()],
   corePlugins: {
     // make sure these are enabled
     transform: true,
     transitionProperty: true,
   },
+  server:{
+    proxy:{
+      '/api':{
+        target:process.env.VITE_BACKENDURL,
+        changeOrigin:true,
+        secure:true
+      }
+    }
+  }
+
 })
