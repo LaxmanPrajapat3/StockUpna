958ac93b6560313823da549d057123bb3539050a
Laxman <theprogrammer452023@gmail.com>
2025-08-15 00:03:46 +0530

    sovle buge on vite.confg.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from "dotenv";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
 dotenv.config();
-// https://vite.dev/config/
+
 export default defineConfig({
-   theme: {
-    extend: {},
-  },
-  base:"./",
   plugins: [react()],
-  corePlugins: {
-    // make sure these are enabled
-    transform: true,
-    transitionProperty: true,
-  },
-  server:{
-    proxy:{
-      '/api':{
-        target:process.env.VITE_BACKENDURL,
-        changeOrigin:true,
-        secure:true
+  base: './',
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_BACKENDURL,
+        changeOrigin: true,
+        secure: true
       }
     }
   }
-
-})
+});
